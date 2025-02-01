9645d88296fc1a482072ff52d1f1627e542c5bef
sankalp nadiger <nadigersankalp@gmail.com>
2025-02-01 18:09:24 +0530

    upload modifications

--- backend/src/routes/teacher.routes.js
+++ backend/src/routes/teacher.routes.js
@@ -1,17 +1,18 @@
 import express from "express";
 import { registerTeacher, addVideo, approveVideo, answerQuestion } from "../controllers/teacherController.js";
-import { authenticate, isAdmin } from "../middleware/authMiddleware.js";
+import { isAdmin } from "../middleware/auth.middleware.js";
 import multer from "multer";
+import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 const upload = multer({ dest: "uploads/" }); 
 
 router.post("/register", registerTeacher);
 
-router.post("/upload-video", authenticate, upload.single("video"), addVideo);
+router.post("/upload-video", verifyJWT, upload.single("video"), addVideo);
 
-router.patch("/approve-video/:videoId", authenticate, isAdmin, approveVideo);
+router.patch("/approve-video/:videoId", verifyJWT, isAdmin, approveVideo);
 
-router.post("/answer-question", authenticate, answerQuestion);
+router.post("/answer-question", verifyJWT, answerQuestion);
 
 export default router;
